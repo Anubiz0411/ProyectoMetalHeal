a949d12a884209dcaee4d0decab19d8d6f0e87cb
Andre Lopez <andres15-14@utp.edu.co>
2016-04-18 12:30:16 -0500

    Cradas las Paginas About y Contac

---
+++ proyectoheal/home/forms.py
@@ -0,0 +1,30 @@
+ # coding=utf-8
+from django import forms
+
+
+class ContactUsuarioAnonimoForm(forms.Form):
+
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    subject = forms.CharField(
+        label='Asunto',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    body = forms.CharField(
+        label='Mensaje',
+        widget=forms.Textarea(attrs={'class': 'form-control'})
+    )
+
+
+class ContactUsuarioLoginForm(forms.Form):
+
+    subject = forms.CharField(
+        label='Asunto',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    body = forms.CharField(
+        label='Mensaje',
+        widget=forms.Textarea(attrs={'class': 'form-control'})
+    )
--- proyectoheal/home/urls.py
+++ proyectoheal/home/urls.py
@@ -1,5 +1,5 @@
 # home/urls.py
-
+ # coding=utf-8
 from django.conf.urls import url
 
 from . import views
@@ -7,4 +7,6 @@ from . import views
 urlpatterns = [
     # /home/
     url(r'^$', view=views.index_view, name='home.index'),
+    url(r'^about/$', view=views.AboutView, name='home.about'),
+    url(r'^contact/$', view=views.ContactView.as_view(), name='home.contact'),
 ]
